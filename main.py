0d73117a8b61f7c6a722520b35045bc487509483
Malachi <malachishaffer@protonmail.com>
2022-06-11 19:44:26 -0400

    added player image

--- main.py
+++ main.py
@@ -6,6 +6,17 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('Bullet hell')
 
+class Player(pygame.sprite.Sprite):
+    '''The Main player class'''
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load('sprites/player/player.png').convert_alpha()
+        self.rect = self.image.get_rect(topleft = (0, 0))
+
+# Player
+player = pygame.sprite.GroupSingle()
+player.add(Player())
+
 # main game loop
 while True:
 
@@ -15,4 +26,5 @@ while True:
             pygame.quit()
             exit()
 
+    player.draw(screen)
     pygame.display.update()
